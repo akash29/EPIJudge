b3fee112d637f78c665df1b379205e4132b09d76
Akash Raghurajan <araghurajan3@gatech.edu>
2019-02-06 22:50:30 -0500

    divide implementation

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -3,7 +3,25 @@ from test_framework import generic_test
 
 def divide(x, y):
     # TODO - you fill in here.
-    return 0
+    if x < y:
+        return 0
+    else:
+        count = 0
+        temp_y = y
+        while x-y >= 0:
+            temp_power = 0
+            while x >= y:
+                y <<= 1
+                if y > x:
+                    y >>= 1
+                    break
+                temp_power += 1
+
+            x = x - y
+            y = temp_y
+            count += 1 << temp_power
+
+        return count
 
 
 if __name__ == '__main__':
